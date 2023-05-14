58371896cf919d439a4d6fee710c71c545c41687
MariamElSahhar <me1693@nyu.edu>
2023-05-14 15:33:24 +0400

    pipe check

--- parsing/src/split.c
+++ parsing/src/split.c
@@ -6,77 +6,54 @@
 /*   By: melsahha <melsahha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 18:39:14 by melsahha          #+#    #+#             */
-/*   Updated: 2023/05/12 14:44:56 by melsahha         ###   ########.fr       */
+/*   Updated: 2023/05/14 15:28:47 by melsahha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/parsing.h"
+
+static int	in_quotes(int *i, char *input)
+{
+	int	quote;
+
+	quote = input[*i];
+	(*i) = (*i) + 1;
+	while (input[*i] && input[*i] != quote)
+		(*i) = (*i) + 1;
+	if (input[*i] && input[*i] == quote)
+	{
+		(*i) = (*i) + 1;
+		return (0);
+	}
+	return (1);
+}
 
 static int	count_commands(char *input)
 {
 	int		i;
 	int		open;
 	int		count;
-	int		quote;
 
 	i = 0;
-	count = 0;
+	count = 1;
+	open = 0;
 	while (input && input[i])
 	{
-		open = 0;
 		while (input[i] && !is_quote(input[i]) && input[i] != '|')
 			i++;
-		if (is_quote(input[i]))
-		{
-			open = 1;
-			quote = input[i];
-			i++;
-		}
+		if (input[i] && is_quote(input[i]))
+			open = in_quotes(&i, input);
 		while (input[i] && !is_quote(input[i]) && input[i] != '|')
 			i++;
-		if (input[i] == quote)
-		{
-			open = 0;
-			i++;
-		}
-		while (input[i] && !is_quote(input[i]) && input[i] != '|')
-			i++;
-		if (input[i] == '|' && !open)
+		if (input[i] && input[i] == '|' && !open && !is_quote(input[i]))
 		{
 			count++;
 			i++;
 		}
-		while (input[i] && !is_quote(input[i]) && input[i] != '|')
-			i++;
 	}
-
 	return (count);
 }
 
-
-/* static int	count_commands(char *s)
-{
-	int	count;
-	int	i;
-
-	i = 0;
-	count = 0;
-	while (s && s[i])
-	{
-		while (s[i] && s[i] != '|')
-			i++;
-		if (s[i] && s[i] == '|')
-		{
-			count++;
-			i++;
-		}
-		while (s[i] && s[i] != '|')
-			i++;
-	}
-	if (!count)
-		return (1);
-	return (count);
-} */
 
 /* static void	init_word(char *d, char *s, int start)
 {
@@ -121,14 +98,14 @@ static void	init_arr(char **arr, char *s)
 char	**split_input(char *input)
 {
 	int		num_commands;
-	// char	**arr;
+	char	**commands_str;
 
 	num_commands = count_commands(input);
 	printf("%i commands\n", num_commands);
+	commands_str = (char **)malloc(sizeof(char *) * (num_commands + 1));
+	if (!commands_str)
+		return (0);
 	return (0);
-	// arr = (char **)malloc(sizeof(char *) * (num_commands + 1));
-	// if (!arr)
-	// 	return (0);
 	// init_arr(arr, (char *) s);
 	// arr[num_commands] = 0;
 	// return (arr);
