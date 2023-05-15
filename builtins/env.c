0a1d6c64e8bc86d3dc8f41adcb76c00c368a3f88
Simon Zerisenay <szerisen@lab1r1s11.42abudhabi.ae>
2023-05-15 19:24:22 +0400

    Added change_path function

--- builtins/env.c
+++ builtins/env.c
@@ -6,13 +6,13 @@
 /*   By: szerisen <szerisen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 17:42:53 by szerisen          #+#    #+#             */
-/*   Updated: 2023/05/15 17:45:45 by szerisen         ###   ########.fr       */
+/*   Updated: 2023/05/15 18:18:10 by szerisen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-int	mini_env(t_utils *utils, t_cmds *cmds)
+int	env(t_utils *utils, t_cmds *cmds)
 {
 	int		i;
 
--- builtins/utils_builtins.c
+++ builtins/utils_builtins.c
@@ -6,7 +6,19 @@
 /*   By: szerisen <szerisen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 17:43:10 by szerisen          #+#    #+#             */
-/*   Updated: 2023/05/10 17:43:11 by szerisen         ###   ########.fr       */
+/*   Updated: 2023/05/15 19:23:08 by szerisen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "builtins.h"
+
+void	change_path(t_utils *utils)
+{
+	char	*tmp;
+
+	tmp = ft_strdup(utils->pwd);
+	free(utils->old_pwd);
+	utils->old_pwd = tmp;
+	free(utils->pwd);
+	utils->pwd = getcwd(NULL, sizeof(NULL));
+}
