abf94c48d75158afe4707b40612fc7ce536e3250
MariamElSahhar <me1693@nyu.edu>
2023-05-28 12:46:52 +0400

    [WIP] expanding env

--- parsing/src/expand_env.c
+++ parsing/src/expand_env.c
@@ -6,17 +6,65 @@
 /*   By: melsahha <melsahha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 11:39:28 by melsahha          #+#    #+#             */
-/*   Updated: 2023/05/28 11:58:28 by melsahha         ###   ########.fr       */
+/*   Updated: 2023/05/28 12:38:37 by melsahha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/parsing.h"
 
+char	*replace_env(char *str, int *i, char *exp, int len)
+{
+	char	*full;
+	int		j;
+	int		k;
+
+	full = (char *)ft_calloc((ft_strlen(str) - len) + ft_strlen(exp) + 2, sizeof(char));
+	if (!full)
+		return (0);
+	j = -1;
+	k = (*i) - len - 1;
+	while (++j < k)
+		full[j] = str[j];
+	k = 0;
+	while (k < (int)ft_strlen(exp))
+		full[j++] = exp[k++];
+	k = (*i);
+	while (k < (int)ft_strlen(str))
+		full[j++] = str[k++];
+	free(str);
+	return (full);
+}
+
+int	found_env(char *str, int *i, t_word *word)
+{
+	int		len;
+	char	*var;
+	int		j;
+	char	*exp;
+
+	len = 0;
+	j = (*i);
+	while (str[++j] && !is_space(str[j])
+		&& !is_quote(str[j]) && str[j] != '$')
+		len++;
+	var = (char *)ft_calloc(len + 1, sizeof(char));
+	if (!var)
+		return (0);
+	j = 0;
+	(*i)++;
+	while (j < len)
+		var[j++] = str[(*i)++];
+	exp = getenv(var);
+	word->cont = replace_env(str, i, exp, len);
+	free(var);
+	if (!word->cont)
+		return (0);
+	return (1);
+}
+
 int	expand_env_str(t_word *word)
 {
 	int		i;
-	int		len;
-	char	*var;
 
 	if (!ft_strchr(word->cont, '$'))
 		return (1);
@@ -25,16 +73,13 @@ int	expand_env_str(t_word *word)
 		i++;
 	while (word->cont[i])
 	{
-		len = 0;
 		if (word->cont[i] == '$')
 		{
-			while (word->cont[++i] && !is_space(word->cont[i])
-				&& !is_quote(word->cont[i]) && word->cont[i] != '$')
-				len++;
-			var = ft_calloc(len + 1, sizeof(char));
-			//FINISH THIS
+			if (!found_env(word->cont, &i, word))
+				return (0);
 		}
-
+		else
+			i++;
 	}
 	return (1);
 }
@@ -49,8 +94,8 @@ int	expand_env(t_split *split)
 	{
 		if (ptr->type == STR || ptr->type == CMD || ptr->type == FLAG || ptr->type == PATH)
 			success = expand_env_str(ptr);
-		// else if (ptr->type == QUOTE && ptr->cont[0] == "\"")
-		// 	success = expand_var_quote(ptr);
+		else if (ptr->type == QUOTE && ptr->cont[0] == '\"')
+			success = expand_var_quote(ptr);
 		if (!success)
 			return (0);
 		ptr = ptr->next;
