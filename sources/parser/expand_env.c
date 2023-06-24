7bf188f030236faa5140ad846902d83e2384e3a2
MariamElSahhar <me1693@nyu.edu>
2023-06-24 14:44:58 +0400

    fix consecutive $

--- sources/parser/expand_env.c
+++ sources/parser/expand_env.c
@@ -6,7 +6,7 @@
 /*   By: melsahha <melsahha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 11:39:28 by melsahha          #+#    #+#             */
-/*   Updated: 2023/06/24 14:30:56 by melsahha         ###   ########.fr       */
+/*   Updated: 2023/06/24 14:44:12 by melsahha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,6 @@ int	found_env(char *str, int *i, t_word *word)
 	exp = getenv(var);
 	if (!exp)
 		return (0);
-	printf("%i and %i\n", *i, len);
 	word->cont = replace_env(str, i, exp, len);
 	free(var);
 	if (!word->cont)
@@ -89,6 +88,7 @@ int	expand_env_str(t_word *word)
 			}
 			else if (!found_env(word->cont, &i, word))
 				return (0);
+			i = 0;
 		}
 		else
 			i++;
